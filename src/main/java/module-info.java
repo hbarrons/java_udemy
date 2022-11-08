aac59196bfb13da690271b47bdeb1bede6131aeb
Howard Barrons <barronsh@locustec.com>
2022-11-08 09:58:01 -0500

    correct file structure in order to run build

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.java_udemy {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.java_udemy to javafx.fxml;
-    exports com.example.java_udemy;
-}
